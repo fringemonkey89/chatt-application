08c65b592bc7e5c6b544832c596ab477af7b5d0d
fringemonkey89 <markomajeticjr@gmail.com>
2023-10-31 10:44:41 -0400

    added code to index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFilr(__dirname + '/index.html', (err, data) => {
+            if(err){
+                res.writeHead(500);
+                res.end('error loading index.html');
+            } else {
+                res.writeHead(200, {'Content-type': 'text/html'});
+                res.end(data)
+            }
+        })
+    }
+});
+
+const io = require('socket.io')(server);
+const port = 5000;
+
+io.on('connection', (socket)=> {
+    socket.io('send name', (user) => {
+        io.emit('send name', user) ;
+    })
+    socket.io('send message', (chat) => {
+        io.emit('send message', chat) ;
+    })
+})
+
+server.listen(port, () => {
+    console.log(`server is listening at the port: ${port}`)
+})
